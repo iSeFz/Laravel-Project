dec895634ca008f56b238c3f2577c5a9dfe63e64
iSeFz <seifyahia@gmail.com>
2024-05-15 17:55:53 +0300

    Added feature test for correct API results

---
+++ tests/Feature/APITest.php
@@ -0,0 +1,21 @@
+<?php
+
+test('API returned correct actors', function () {
+    $birthdate = '1980-01-01'; // Replace this with the actual birthdate
+    $response = $this->get('/check-actors?birthdate=' . urlencode($birthdate));
+    
+    $jsonResponse = $response->json();
+    $expectedResponse = array(
+        0 => "Jessica Gunning",
+        1 => "Angourie Rice",
+        2 => "Cameron Cowperthwaite",
+        3 => "Stanley Kamel",
+        4 => "Colin Morgan"
+    );
+    
+    // Print the response to the console
+    // error_log(print_r($jsonResponse, true));
+
+    $response->assertStatus(200);
+    $this->assertEquals($expectedResponse, $jsonResponse);
+});
